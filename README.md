6bc25d0ba170f13c0b42d5c684f140796edf1901
zyxterus <176840031+zyxterus@users.noreply.github.com>
2025-11-28 13:41:58 +0100

    feat: setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Self Hosted Photon
+
+With this mod you can easily join to a self-hosted version of Photon via an IP address or a hostname.
---
+++ Self_Hosted_Photon/Self_Hosted_Photon.cs
@@ -0,0 +1,32 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using REPOLib;
+using UnityEngine;
+
+namespace Self_Hosted_Photon;
+
+/// <summary>
+/// Self_Hosted_Photon's main plugin class.
+/// </summary>
+[BepInPlugin(MyPluginInfo.PLUGIN_GUID, MyPluginInfo.PLUGIN_NAME, MyPluginInfo.PLUGIN_VERSION)]
+[BepInDependency(REPOLib.MyPluginInfo.PLUGIN_GUID, BepInDependency.DependencyFlags.HardDependency)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static Plugin Instance { get; private set; } = null!;
+    internal static new ManualLogSource Logger => Instance._logger;
+
+    internal static string PluginFolder => System.IO.Path.GetDirectoryName(Instance.Info.Location)!;
+
+    private ManualLogSource _logger => base.Logger;
+
+    private void Awake()
+    {
+        Instance = this;
+
+        // Prevent the plugin from being deleted
+        this.gameObject.transform.parent = null;
+        this.gameObject.hideFlags = HideFlags.HideAndDontSave;
+
+        Logger.LogInfo($"{Info.Metadata.GUID} v{Info.Metadata.Version} has loaded!");
+    }
+}
