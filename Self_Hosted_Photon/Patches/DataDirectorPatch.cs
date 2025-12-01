401f6a04b4bd061494e5b699755e44c823ea50fe
zyxterus <176840031+zyxterus@users.noreply.github.com>
2025-12-01 21:19:15 +0100

    feat: add photon setting creation patch

---
+++ Self_Hosted_Photon/Patches/DataDirectorPatch.cs
@@ -0,0 +1,31 @@
+﻿using ExitGames.Client.Photon;
+using HarmonyLib;
+using Photon.Pun;
+
+namespace Self_Hosted_Photon.Patches;
+
+[HarmonyPatch(typeof(DataDirector))]
+internal static class DataDirectorPatch
+{
+    [HarmonyPatch(nameof(DataDirector.PhotonSetAppId))]
+    [HarmonyPrefix]
+    private static bool PhotonSetAppIdPrefix()
+    {
+        if (!ConfigManager.Enabled.Value)
+            return true;
+
+        PhotonNetwork.NetworkingClient.SerializationProtocol = SerializationProtocol.GpBinaryV16;
+
+        PhotonNetwork.PhotonServerSettings.AppSettings.AppIdRealtime = string.Empty;
+        PhotonNetwork.PhotonServerSettings.AppSettings.AppIdVoice = string.Empty;
+
+        var serverAddress = ConfigManager.ServerAddress.Value;
+        var serverPort = ushort.Parse(ConfigManager.ServerPort.Value);
+        PhotonNetwork.PhotonServerSettings.AppSettings.Server = serverAddress;
+        PhotonNetwork.PhotonServerSettings.AppSettings.Port = serverPort;
+
+        PhotonNetwork.PhotonServerSettings.AppSettings.UseNameServer = false;
+
+        return false;
+    }
+}
