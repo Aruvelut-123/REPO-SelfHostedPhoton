a65d9796688b32064b935d430a69fd84c5a81db4
zyxterus <176840031+zyxterus@users.noreply.github.com>
2025-12-01 21:18:50 +0100

    feat: add server address and port configs

--- Self_Hosted_Photon/ConfigManager.cs
+++ Self_Hosted_Photon/ConfigManager.cs
@@ -8,6 +8,9 @@ internal static class ConfigManager
 
     public static ConfigEntry<bool> Enabled { get; private set; } = null!;
 
+    public static ConfigEntry<string> ServerAddress { get; private set; } = null!;
+    public static ConfigEntry<string> ServerPort { get; private set; } = null!;
+
     public static void Initialize(ConfigFile configFile)
     {
         ConfigFile = configFile;
@@ -21,6 +24,20 @@ internal static class ConfigManager
             "Enabled",
             defaultValue: true,
             "Enable or disable the plugin."
+        );
+
+        ServerAddress = ConfigFile.Bind(
+            "Server",
+            "Address",
+            defaultValue: "localhost",
+            "The IP address or hostname of the Photon server."
+        );
+
+        ServerPort = ConfigFile.Bind(
+            "Server",
+            "Port",
+            defaultValue: "5055",
+            "The port number of the Photon server."
         );
     }
 }
